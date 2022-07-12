11d93014427f4a982d441e0b0180a55823c02cae
Prachhi Sabita Mishra <62738461+PSM1729@users.noreply.github.com>
2022-07-12 23:01:25 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3){
+           map<int,int> mp1;
+           map<int,int> mp2;
+           set<int> ans;
+           for(int i=0;i<n1;i++){
+               mp1[A[i]]++;
+           }
+           for(int i=0;i<n2;i++){
+               mp2[B[i]]++;
+           }
+           for(int i=0;i<n3;i++){
+               if(mp1[C[i]] && mp2[C[i]])
+                    ans.insert(C[i]);
+           }
+           return vector<int>(ans.begin(),ans.end());
+        }
+
+};
+
+// { Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}  // } Driver Code Ends
