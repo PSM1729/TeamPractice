30baf1274389baabd77767f23804aad9524699cd
Prachhi Sabita Mishra <62738461+PSM1729@users.noreply.github.com>
2022-07-23 23:53:24 +0530

    Time: 806 ms (33.33%), Space: 176.9 MB (33.33%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -0,0 +1,32 @@
+class NumberContainers {
+public:
+    unordered_map<int,set<int>> umap;
+    unordered_map<int,int> ind;
+    NumberContainers() {
+        umap.clear();
+        ind.clear();
+    }
+    
+    void change(int index, int number) {
+        if(ind.find(index)!=ind.end()){
+            umap[ind[index]].erase(index);
+            ind[index] = number;
+        }
+        else
+            ind[index] = number;
+            umap[number].insert(index); 
+    }
+    
+    int find(int number) {
+        if(umap.find(number)==umap.end() || umap[number].size()==0)
+            return -1;
+        return *umap[number].begin();
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
